982de632702b1e86786b1472b4822401a8f51494
sudhandeveloper <gmsudhan3@gmail.com>
2024-03-07 19:05:03 +0530

    new

--- test-project/src/views/commponents/auth-components/Sign-Up.js
+++ test-project/src/views/commponents/auth-components/Sign-Up.js
@@ -1,10 +1,8 @@
 import * as React from "react";
-import Avatar from "@mui/material/Avatar";
+
 import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";
 import TextField from "@mui/material/TextField";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import Checkbox from "@mui/material/Checkbox";
 import { Link, useNavigate } from "react-router-dom";
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
--- test-project/src/views/pages/employee-form-page/employee-form.jsx
+++ test-project/src/views/pages/employee-form-page/employee-form.jsx
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import Radio from "@mui/material/Radio";
 import RadioGroup from "@mui/material/RadioGroup";
 import FormControlLabel from "@mui/material/FormControlLabel";
-import FormControl from "@mui/material/FormControl";
 import FormLabel from "@mui/material/FormLabel";
 import TextField from "@mui/material/TextField";
 import Autocomplete from "@mui/material/Autocomplete";
